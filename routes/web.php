57d4d3b0980687b96915569fc645a0c409604bd4
= <=>
2016-09-27 10:49:33 -0700

    Implement application routing

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of the routes that are handled
-| by your application. Just tell Laravel the URIs it should respond
-| to using a Closure or controller method. Build something great!
-|
-*/
+// Define all routes for the l5_blog project
+Route::get('/', 'BlogController@index');
+Route::get('/blog/add', 'BlogController@add');
+Route::get('/blog/delete/{id}', 'BlogController@delete');
+Route::get('/blog/display', 'BlogController@display');
+Route::get('/blog/edit/{id}', 'BlogController@edit');
+Route::get('/blog/save', 'BlogController@save');
+Route::get('/blog/save/{id}', 'BlogController@save');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Define a route to match all other requests
+// This should be the last rule in your routing file(s)
+Route::any('{all}', 'BlogController@index')->where('all', '.*');
